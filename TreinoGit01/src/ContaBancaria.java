a4bf35c483d66212ec7460ffe1bb3c9e29147636
Dan-dev1305 <Administrator@ROMERITO.AD>
2025-06-20 11:30:33 -0300

    Entrada de dados na main e preparação dos métodos na conta

--- TreinoGit01/src/ContaBancaria.java
+++ TreinoGit01/src/ContaBancaria.java
@@ -1,5 +1,14 @@
 public class ContaBancaria {
     String nomeTitular;
-    
+    double saldo;
+    double deposito;
+    int numeroConta;
+
+    double depositoConta(){
+        return saldo += deposito;
+    }
+
+
+
 
 }
